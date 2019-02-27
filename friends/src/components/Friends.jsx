aea685409d4c033794808af0013474c679a99375
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-27 15:02:52 +0100

    feat: Add Friends Component that fetches data and style it

---
+++ friends/src/components/Friends.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+const StyledFriends = styled.div`
+  width: 50vw;
+  border-radius: 10px;
+  background-color: white;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  padding: 10px;
+`;
+
+const StyledFriend = styled.div`
+  border-radius: 10px;
+  background: linear-gradient(to right, #5d26c1, #a17fe0, #59c173);
+  color: white;
+  padding: 10px;
+  margin: 10px;
+  width: 20vw;
+`;
+
+export default class Friends extends React.Component {
+  state = {
+    friends: null,
+    error: null,
+    loading: false
+  };
+
+  componentDidMount() {
+    this.fetchFriends();
+  }
+
+  fetchFriends = () => {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(res => this.addFriends(res.data))
+      .catch(res => this.addError(res.message));
+  };
+  addFriends = friends => {
+    this.setState({ friends: friends });
+  };
+
+  addError = error => {
+    this.setState({ error: error });
+  };
+
+  render() {
+    console.log(this.state.friends);
+    return (
+      <div>
+        <StyledFriends>
+          {this.state.friends &&
+            this.state.friends.map(friend => {
+              return (
+                <StyledFriend key={friend.name}>
+                  <div>Name: {friend.name}</div>
+                  <div>Age: {friend.age}</div>
+                  <div>Email: {friend.email}</div>
+                </StyledFriend>
+              );
+            })}
+          {!this.state.friends && <div>Loading friends...</div>}
+        </StyledFriends>
+      </div>
+    );
+  }
+}
